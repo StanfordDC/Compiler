535c04bb0486e3637a8752b5e06ae0c5ff95cd1b
StanfordDC <stanforddchandra@gmail.com>
2022-09-14 00:02:21 +0800

    Debugged

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int, char**) {
     std::cout << "Bytecode interpreter\n";
 
     Instruction code[] = {
-        Instruction{PUSH_INT, 0, 4000};
-        Instruction{PUSH_INT, 0, 1042};
-        Instruction{ADD_INT, 0, 0};
-    }
+        Instruction{PUSH_INT, 0, 4000},
+        Instruction{PUSH_INT, 0, 1042},
+        Instruction{ADD_INT, 0, 0}
+    };
 }
