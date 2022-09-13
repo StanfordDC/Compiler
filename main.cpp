617ee8ce2e27e48d2755f9c7139b2bed7b45ff5d
StanfordDC <stanforddchandra@gmail.com>
2022-09-14 00:01:46 +0800

    Added test case

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <Instructions.cpp>
 
 int main(int, char**) {
     std::cout << "Bytecode interpreter\n";
+
+    Instruction code[] = {
+        Instruction{PUSH_INT, 0, 4000};
+        Instruction{PUSH_INT, 0, 1042};
+        Instruction{ADD_INT, 0, 0};
+    }
 }
