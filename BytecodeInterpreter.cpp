56f8be983756bb037e5d794acca85469f5e64b01
StanfordDC <stanforddchandra@gmail.com>
2022-09-14 22:54:51 +0800

    Added new functions

--- BytecodeInterpreter.cpp
+++ BytecodeInterpreter.cpp
@@ -2,7 +2,35 @@
 
 namespace bytecodeInterpreter{
 
-    void BytecodeInterpreter::run(Instruction* code){
+    extern InstructionFunction gInstructionFunctions[NUM_INSTRUCTIONS] = {
+        ExitInstruction,
+        AddIntInstruction,
+        PushIntInstruction,
+        PrintIntInstruction
+    };
 
+    void BytecodeInterpreter::run(Instruction* code){
+        InterpreterRegisters registers;
+        registers.currentInstruction = code;
+
+        while(registers.currentInstruction != nullptr){
+            gInstructionFunctions[registers.currentInstruction->opcode](registers);
+        }
+    }
+
+    void ExitInstruction(InterpreterRegisters& registers){
+        
+    }
+
+    void AddIntInstruction(InterpreterRegisters& registers){
+        
+    }
+
+    void PushIntInstruction(InterpreterRegisters& registers){
+        
+    }
+
+    void PrintIntInstruction(InterpreterRegisters& registers){
+        
     }
 }
