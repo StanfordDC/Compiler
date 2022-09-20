035423ba2459342e70a07bc55ed7595da64a3b92
StanfordDC <stanforddchandra@gmail.com>
2022-09-20 21:50:39 +0800

    Clear main function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-#include "Instructions.cpp"
-#include <vector>
 
 using namespace std;
 
 int main(int, char**) {
-    std::cout << "Bytecode interpreter\n";
-
-    Instruction code[] = {
-        Instruction{PUSH_INT, 0, 4000},
-        Instruction{PUSH_INT, 0, 1042},
-        Instruction{ADD_INT, 0, 0},
-    };
-
+    
 }
