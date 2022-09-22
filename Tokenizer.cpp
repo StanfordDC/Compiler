d1c5eb6a139f65da9d04014d52dab8ab51d1c951
StanfordDC <stanforddchandra@gmail.com>
2022-09-23 00:37:02 +0800

    Implemented tokenizer

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -1,52 +1,168 @@
-#include <Tokenizer.h>
+#include "Tokenizer.hpp"
+#include <stdexcept>
+#include <iostream>
 
-using namespace std;
+namespace simpleparser {
 
-vector<Token> Tokenizer::parse(const string &inProgram){
-    vector<Token> tokens;
-    Token currentToken;
+    using namespace std;
 
-    for(char c : inProgram){
-        switch(c){
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                if (currentToken.mType == WHITESPACE) {
-                    currentToken.mType = INTEGER_LITERAL;
-                    currentToken.mText.append(1, c);
-                } else {
-                    currentToken.mText.append(1, c);
+    vector<Token> Tokenizer::parse(const std::string &inProgram) {
+        vector<Token> tokens;
+        Token currentToken;
+
+        currentToken.mLineNumber = 1;
+
+        for (char currCh : inProgram) {
+            if (currentToken.mType == STRING_ESCAPE_SEQUENCE) {
+                switch (currCh) {
+                    case 'n':
+                        currentToken.mText.append(1, '\n');
+                        break;
+                    case 'r':
+                        currentToken.mText.append(1, '\r');
+                        break;
+                    case 't':
+                        currentToken.mText.append(1, '\t');
+                        break;
+                    case '\\':
+                        currentToken.mText.append(1, '\\');
+                        break;
+                    default:
+                        throw runtime_error(string("unknown escape sequence: \\") + string(1, currCh) +
+                                            " in string on line " + to_string(currentToken.mLineNumber) + ".");
+                        break;
                 }
-                break;
-                
-            case '{':
-            case '}':
-            case '(':
-            case ')':
-            case '=':
-            case '+':
-            case '-':
-            case '*':
-            case '<':
-            case ';':
-            case ',':
-                if (currentToken.mType != STRING_LITERAL) {
-                    endToken(currentToken, tokens);
-                    currentToken.mType = OPERATOR;
-                    currentToken.mText.append(1, currCh);
-                    endToken(currentToken, tokens);
-                } else {
-                    currentToken.mText.append(1, currCh);
-                }
-                break;
+                currentToken.mType = STRING_LITERAL;
+                continue;
+            } else if (currentToken.mType == POTENTIAL_COMMENT && currCh != '/') {
+                currentToken.mType = OPERATOR;
+                endToken(currentToken, tokens);
+                continue;
+            }
 
+            switch (currCh) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    if (currentToken.mType == WHITESPACE) {
+                        currentToken.mType = INTEGER_LITERAL;
+                        currentToken.mText.append(1, currCh);
+                    } else if (currentToken.mType == POTENTIAL_DOUBLE) {
+                        currentToken.mType = DOUBLE_LITERAL;
+                        currentToken.mText.append(1, currCh);
+                    } else {
+                        currentToken.mText.append(1, currCh);
+                    }
+                    break;
+
+                case '.':
+                    if (currentToken.mType == WHITESPACE) {
+                        currentToken.mType = POTENTIAL_DOUBLE;
+                        currentToken.mText.append(1, currCh);
+                    } else if (currentToken.mType == INTEGER_LITERAL) {
+                        currentToken.mType = DOUBLE_LITERAL;
+                        currentToken.mText.append(1, currCh);
+                    } else if (currentToken.mType == STRING_LITERAL) {
+                        currentToken.mText.append(1, currCh);
+                    } else {
+                        endToken(currentToken, tokens);
+                        currentToken.mType = OPERATOR;
+                        currentToken.mText.append(1, currCh);
+                        endToken(currentToken, tokens);
+                    }
+                    break;
+
+                case '{':
+                case '}':
+                case '(':
+                case ')':
+                case '=':
+                case '+':
+                case '-':
+                case '*':
+                case '<':
+                case ';':
+                case ',':
+                    if (currentToken.mType != STRING_LITERAL) {
+                        endToken(currentToken, tokens);
+                        currentToken.mType = OPERATOR;
+                        currentToken.mText.append(1, currCh);
+                        endToken(currentToken, tokens);
+                    } else {
+                        currentToken.mText.append(1, currCh);
+                    }
+                    break;
+
+                case ' ':
+                case '\t':
+                    if (currentToken.mType == STRING_LITERAL || currentToken.mType == COMMENT) {
+                        currentToken.mText.append(1, currCh);
+                    } else {
+                        endToken(currentToken, tokens);
+                    }
+                    break;
+
+                case '\r':
+                case '\n':
+                    endToken(currentToken, tokens);
+                    ++currentToken.mLineNumber;
+                    break;
+
+                case '"':
+                    if (currentToken.mType != STRING_LITERAL) {
+                        endToken(currentToken, tokens);
+                        currentToken.mType = STRING_LITERAL;
+                    } else if (currentToken.mType == STRING_LITERAL) {
+                        endToken(currentToken, tokens);
+                    }
+                    break;
+
+                case '\\':
+                    if (currentToken.mType == STRING_LITERAL) {
+                        currentToken.mType = STRING_ESCAPE_SEQUENCE;
+                    } else {
+                        endToken(currentToken, tokens);
+                        currentToken.mType = OPERATOR;
+                        currentToken.mText.append(1, currCh);
+                        endToken(currentToken, tokens);
+                    }
+                    break;
+
+                case '/':
+                    if (currentToken.mType == STRING_LITERAL) {
+                        currentToken.mText.append(1, currCh);
+                    } else if (currentToken.mType == POTENTIAL_COMMENT) {
+                        currentToken.mType = COMMENT;
+                        currentToken.mText.erase();
+                    } else {
+                        endToken(currentToken, tokens);
+                        currentToken.mType = POTENTIAL_COMMENT;
+                        currentToken.mText.append(1, currCh);
+                    }
+                    break;
+
+                default:
+                    if (currentToken.mType == WHITESPACE || currentToken.mType == INTEGER_LITERAL
+                                                            || currentToken.mType == DOUBLE_LITERAL) {
+                        endToken(currentToken, tokens);
+                        currentToken.mType = IDENTIFIER;
+                        currentToken.mText.append(1, currCh);
+                    } else {
+                        currentToken.mText.append(1, currCh);
+                    }
+                    break;
+            }
         }
+
+        endToken(currentToken, tokens);
+
+        return tokens;
     }
 }
