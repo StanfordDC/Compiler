557388a0b076d34d0992bad9ac4eadfffabffa2d
StanfordDC <stanforddchandra@gmail.com>
2022-09-23 00:38:46 +0800

    Added namespace

--- Tokenizer.h
+++ Tokenizer.h
@@ -3,30 +3,33 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+namespace simpleparser{
+    
+    using namespace std;
 
-enum TokenType{
-    WHITESPACE, // No token ever has this type.
-    IDENTIFIER,
-    INTEGER_LITERAL,
-    DOUBLE_LITERAL,
-    STRING_LITERAL,
-    OPERATOR,
-    STRING_ESCAPE_SEQUENCE,
-    STRING_ESCAPE_SEQUENCE,
-    POTENTIAL_DOUBLE,
-    POTENTIAL_COMMENT,
-    COMMENT
-};
+    enum TokenType{
+        WHITESPACE, // No token ever has this type.
+        IDENTIFIER,
+        INTEGER_LITERAL,
+        DOUBLE_LITERAL,
+        STRING_LITERAL,
+        OPERATOR,
+        STRING_ESCAPE_SEQUENCE,
+        STRING_ESCAPE_SEQUENCE,
+        POTENTIAL_DOUBLE,
+        POTENTIAL_COMMENT,
+        COMMENT
+    };
 
-class Token{
-public:
-     enum TokenType mType{WHITESPACE};
-    string mText;
-    size_t mLineNumber{0};
-};
+    class Token{
+    public:
+        enum TokenType mType{WHITESPACE};
+        string mText;
+        size_t mLineNumber{0};
+    };
 
-class Tokenizer{
-public:
-    vector<Token> parse(const string &inProgram);
-};
+    class Tokenizer{
+    public:
+        vector<Token> parse(const string &inProgram);
+    };
+}
