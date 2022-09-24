29b255dffd807b18645a9b956651cd1a229618f1
StanfordDC <stanforddchandra@gmail.com>
2022-09-25 00:59:08 +0800

    Statements to indicate grammars

---
+++ Parser/Statement.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Type.h"
+
+namespace simpleparser {
+
+    using namespace std;
+
+    enum class StatementKind: int {
+        VARIABLE_DECLARATION,
+        FUNCTION_CALL,
+        LITERAL,
+        OPERATOR_CALL,
+        VARIABLE_NAME,
+        WHILE_LOOP
+    };
+
+    static const char* sStatementKindStrings[] = {
+        "VARIABLE_DECLARATION",
+        "FUNCTION_CALL",
+        "LITERAL",
+        "OPERATOR_CALL",
+        "VARIABLE_NAME",
+        "WHILE_LOOP"
+    };
+
+    class Statement {
+    public:
+        string mName;
+        Type mType{Type("void", VOID)};
+        vector<Statement> mParameters;
+        StatementKind mKind{StatementKind::FUNCTION_CALL};
+
+        void debugPrint(size_t indent);
+    };
+}
