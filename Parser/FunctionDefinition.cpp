f2f3b710ed21d9aeaa13a794ab27dfa0e714661b
StanfordDC <stanforddchandra@gmail.com>
2022-09-25 01:20:34 +0800

    Implementation

--- Parser/FunctionDefinition.cpp
+++ Parser/FunctionDefinition.cpp
@@ -1,1 +1,25 @@
+#include "FunctionDefinition.h"
+#include <iostream>
 
+namespace simpleparser {
+
+    using namespace std;
+
+    void FunctionDefinition::debugPrint() const {
+        cout << (mReturnsSomething ? "int " : "void ") << mName << "(\n";
+
+        for (ParameterDefinition param : mParameters) {
+            param.debugPrint(1);
+        }
+
+        cout << ") {\n";
+        for (Statement statement : mStatements) {
+            statement.debugPrint(0);
+        }
+        cout << "}" << endl;
+    }
+
+    void ParameterDefinition::debugPrint(size_t indent) const {
+        cout << string(indent, '\t') << mType.mName << " " << mName << endl;
+    }
+}
